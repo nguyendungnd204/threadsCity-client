0d58aac6cd007e7578f0fb4baf6a8e7b6ca1250f
HoangDuongHuy <duonghuyhoanghuce@gmail.com>
2025-04-08 22:45:41 +0700

    Login FB

--- App.tsx
+++ App.tsx
@@ -11,21 +11,27 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CreateScreens from './src/screens/CreateScreens'
 import TabsNavigation from './src/navigation/TabsNavigation';
+import LoginScreens from './src/screens/Login/LoginScreens';
 
 const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Login">
         <Stack.Screen 
-          name="Tab" 
+          name="Tabs" 
           component={TabsNavigation} 
           options={{animation: 'slide_from_bottom'}}
           />
         <Stack.Screen 
           name="Create" 
           component={CreateScreens}
+          options={{animation: 'slide_from_bottom'}}
+          />
+        <Stack.Screen
+          name="Login" 
+          component={LoginScreens}
           options={{animation: 'slide_from_bottom'}}
           />
       </Stack.Navigator>
