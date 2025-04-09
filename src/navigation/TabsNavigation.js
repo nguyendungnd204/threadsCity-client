e8ca0cef64d184e51fd8a052974fa56eadf6b7ee
HoangDuongHuy <duonghuyhoanghuce@gmail.com>
2025-04-09 21:27:43 +0700

    tach TabIcons

--- src/navigation/TabsNavigation.js
+++ src/navigation/TabsNavigation.js
@@ -7,41 +7,9 @@ import SearchScreens from '../screens/SearchScreens'
 import CreateScreens from '../screens/CreateScreens'
 import { useNavigation } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
 const Tab = createBottomTabNavigator();
-const iconMap = {
-    home: {
-      active: require('../assets/images/home-clicked.png'),
-      inactive: require('../assets/images/home.png'),
-    },
-    user: {
-      active: require('../assets/images/user-clicked.png'),
-      inactive: require('../assets/images/user.png'),
-    },
-    favorite: {
-      active: require('../assets/images/heart-clicked.png'),
-      inactive: require('../assets/images/heart.png'),
-    },
-    create: {
-        active: require('../assets/images/plus.png'),
-        inactive: require('../assets/images/plus.png'),
-    },
-    search: {
-        active: require('../assets/images/search.png'),
-        inactive: require('../assets/images/search.png'),
-    },
-  };
-const IconTabs = ({ focused, color, size, name }) => {
-  const iconSource = focused ? iconMap[name].active : iconMap[name].inactive;
-
-  return (
-    <Image
-      source={iconSource}
-      style={{ width: size, height: size, tintColor: focused ? 'black' : 'gray', }}
-      resizeMode="contain"
-
-    />
-  );
-};
+import IconTabs from '../components/CustomIcon'
 const TabsNavigation = () => {
     const navigation = useNavigation();
   return (
@@ -95,7 +63,7 @@ const TabsNavigation = () => {
                 tabBarIcon: ({ focused, color, size }) => (
                     <IconTabs 
                         focused={focused} 
-                        color={color} // example custom focused color
+                        color={color} 
                         size={size} 
                         name="create" 
                     />
